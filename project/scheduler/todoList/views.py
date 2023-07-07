05ec9b1a2f7d881aa696b22de12b5ce728409b42
James Kim <jamesykim0426@gmail.com>
2023-07-07 09:31:30 -0700

    added a schedule model

--- project/scheduler/todoList/views.py
+++ project/scheduler/todoList/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def main(request):
-    template = loader.get_template('main.html')
+def index(request):
+    template = loader.get_template('index.html')
     return HttpResponse(template.render())
